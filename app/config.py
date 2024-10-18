d4af1ed88019240d86c20b5aa55e9821b0d08210
crigerr <vladimirv2312@gmail.com>
2024-10-18 13:00:26 +0300

    1

--- app/config.py
+++ app/config.py
@@ -1,7 +1,12 @@
-TOKEN='7570207199:AAFpnnbqhotSV5WYzVV2qE3Zay8yGV8m410'
+
 from aiogram.types import BotCommand
 bot_commands = [
     BotCommand(command="/help", description="Get info about me"),
     BotCommand(command="/qna", description="set bot for a QnA task"),
     BotCommand(command="/chat", description="set bot for free chat")
     ]
+
+statuses = {
+'status_text':0,
+'status_word':0,
+}
